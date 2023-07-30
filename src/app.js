b67d87913be29d62f51a9d776484b4469cc049c0
andie7 <131781648+andie7@users.noreply.github.com>
2023-07-30 18:33:43 +0200

    set up search engine

--- src/app.js
+++ src/app.js
@@ -41,7 +41,20 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Paris");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);

756ba687fcc5f4cb5e180b046db4319171cbbf36
andie7 <131781648+andie7@users.noreply.github.com>
2023-08-06 19:29:39 +0200

    forecast with days of the week

--- src/app.js
+++ src/app.js
@@ -7,29 +7,42 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      ` <div class="col-2">
-             // <div class="weather-forecast-date">
-                ${forecastDay.dt}
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+              <div class="weather-forecast-date">
+                ${formatDay(forecastDay.dt)}
               </div>
               <img
-            src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt="sunny"
             id="icon"
           />
           <div class="weather-forecast-temperature">
-          ${forecastDay.temp.max}°|${forecastDay.temp.min}°
+          ${Math.round(forecastDay.temp.max)}°|${Math.round(
+          forecastDay.temp.min
+        )}°
           </div>
             </div>
          `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
